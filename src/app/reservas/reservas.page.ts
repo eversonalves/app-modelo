1afbaeadc8adc2887a17733139d8f4d24ae8ccf3
Everson Alves <everson.alves18@hotmail.com>
2025-11-18 10:34:11 -0300

    finalizando formulario de reserva.

--- src/app/reservas/reservas.page.ts
+++ src/app/reservas/reservas.page.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonFooter, IonButton, IonTabBar, IonTabButton, IonIcon, IonBadge, IonLabel, IonList, IonItem, IonInput, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonDatetime } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonFooter, IonButton, IonTabBar, IonTabButton, IonIcon, IonBadge, IonLabel, IonList, IonItem, IonInput, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonDatetime, IonGrid, IonRow, IonCol, IonAlert } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-reservas',
   templateUrl: './reservas.page.html',
   styleUrls: ['./reservas.page.scss'],
   standalone: true,
-  imports: [IonDatetime, IonCardSubtitle, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonInput, IonItem, IonList, IonLabel, IonBadge, IonIcon, IonTabButton, IonTabBar, IonButton, IonFooter, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton]
+  imports: [IonAlert, IonCol, IonRow, IonGrid, IonDatetime, IonCardSubtitle, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonInput, IonItem, IonList, IonLabel, IonBadge, IonIcon, IonTabButton, IonTabBar, IonButton, IonFooter, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton]
 })
 export class ReservasPage implements OnInit {
+customCounterFormatter: any;
+setOpen(arg0: boolean) {
+throw new Error('Method not implemented.');
+}
+isAlertOpen: any;
+alertButtons: any;
 
   constructor() { }
 
